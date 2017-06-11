9ebee9fa1e5a91a4a13d68cfd8e0bc1d44c00c7e
Simon Thel <s.thel@tu-braunschweig.de>
2017-06-11 19:40:29 +0200

    Dokumentation
    zusätzlich: Vorbereitung zum Einbau der Strömungsfunktion in den Plotter

--- src/plotter.cpp
+++ src/plotter.cpp
@@ -39,8 +39,8 @@ Plotter::~Plotter()
 void Plotter::erstellePlot(Rohr rohr, Fluid fluid)
 {
     Rohrstroemung rohrstroemung(rohr, fluid);
-    ///Temperaturprofil
-    ///Anlegen der Länge als Variable damit Koordinatensystem und for-Schleife angepasst werde
+
+    /// Temperaturprofil
     double l = rohr.get_laenge();
     /// initilaisieren von QVectoren mit Einträgen von 0..100
     QVector<double> x(101), y(101);
@@ -60,20 +60,15 @@ void Plotter::erstellePlot(Rohr rohr, Fluid fluid)
     ui->tempGraph->yAxis->setLabel("Temperatur des Fluids in [K]");
 
     /// Achsenbereich
-    ///x-Achse geht von 0 bis zur Länge des Rohres
     ui->tempGraph->xAxis->setRange(0, l);
 
-    ///Unterscheidung ob t_aussen oder t_ein größer ist; entsprechendes Setzen der y-Achsenbereichs
     if (rohr.get_t_aussen() >= fluid.get_t_ein()){
 
-        ///Bestimmung eines Wertes damit die größten/kleinsten Werte nicht am Rand liegen
         double axis_plus = (rohr.get_t_aussen()-fluid.get_t_ein())/10;
 
-        ///y-Achse beginnt kurz unter t_ein und endet kurz über t_aussen
         ui->tempGraph->yAxis->setRange(fluid.get_t_ein() - axis_plus, rohr.get_t_aussen() + axis_plus);
     }
 
-    ///Analoges Vorgehen wie im if-Statement \sa if(rohr.get_t_aussen >= fluid.get_t_ein())
     else{
         double axis_plus = (fluid.get_t_ein()-rohr.get_t_aussen())/10;
         ui->tempGraph->yAxis->setRange(rohr.get_t_aussen() -axis_plus, fluid.get_t_ein()+axis_plus);
@@ -82,73 +77,102 @@ void Plotter::erstellePlot(Rohr rohr, Fluid fluid)
 
 
 
-
     /// Druckgradient
     QVector<double> g(101), m(101);
-    for (int i=0; i<101; ++i)
     {
-      g[i] = i/50.0 - 1;
-      m[i] = 0.6-g[i];
     }
 
     ui->druckGraph->addGraph();
     ui->druckGraph->graph(0)->setData(g, m);
 
-    ui->druckGraph->xAxis->setLabel("x");
-    ui->druckGraph->yAxis->setLabel("y");
 
-    ui->druckGraph->xAxis->setRange(-1, 1);
-    ui->druckGraph->yAxis->setRange(0, 1);
     ui->druckGraph->replot();
 
 
 
 
     /// Strömungsprofil
-    ui->speedGraph->setInteractions(QCP::iRangeDrag|QCP::iRangeZoom); // Möglichkeit die Farbskala durch Dragging oder Zoomen zu ändern
+    /// Möglichkeit die Farbskala durch Dragging oder Zoomen zu ändern
+    ui->speedGraph->setInteractions(QCP::iRangeDrag|QCP::iRangeZoom);
     ui->speedGraph->axisRect()->setupFullAxesBox(true);
-    ui->speedGraph->xAxis->setLabel("x");
-    ui->speedGraph->yAxis->setLabel("y");
 
-    // aufstellen der QCPColorMap: Wir verwenden die Variablen s (normalerweise x) t (normalerweise y) und z (normalerweise z) damit dies nicht mit den Vatriablen aus
-    // dem Temperaturprofil kollidiert
+    /// Achsenbeschriftung
+    ui->speedGraph->xAxis->setLabel("Länge in [m]");
+    ui->speedGraph->yAxis->setLabel("Radius in [m]");
+
+    /*!
+     * \brief aufstellen der QCPColorMap
+     * \warning  Wir verwenden die Variablen s (normalerweise x) t (normalerweise y) und u (normalerweise z) damit dies nicht mit den Vatriablen aus
+        dem Temperaturprofil kollidiert
+     */
     QCPColorMap *colorMap = new QCPColorMap(ui->speedGraph->xAxis, ui->speedGraph->yAxis);
     int ns = 200;
     int nt = 200;
-    colorMap->data()->setSize(ns, nt); // die ColorMap wird ns*nt groß sein
-    colorMap->data()->setRange(QCPRange(-4, 4), QCPRange(-4, 4)); // Achsenbandbreit (-4..4) wird s und t zugewiesen
-    // Machen wir mal eine coole multivairate Funktion
-    double s, t, u;
+
+    /*! \brief Größe der ColorMap wird festgelegt
+     *
+     * Die ColorMap wird ns*nt groß sein bzw. Bildpunkte haben.
+     * Der Achsberecheich für die x-Achse geht von 0 über die gesamte Länge des Rohres. Der Achsbereich für die y-Achse geht über den Gesamtenquerschnitt (von -r bis r)
+     */
+    colorMap->data()->setSize(ns, nt);
+    colorMap->data()->setRange(QCPRange(0, rohr.get_laenge()), QCPRange((-1)*rohr.get_radius(), rohr.get_radius()));
+
+    /*!
+     * \brief Iterartion über die Strömungsfunktion und Speicherung der Werte in einer Cell.
+     * \warning  Wir verwenden die Variablen s (normalerweise x) t (normalerweise y) und u (normalerweise z) damit dies nicht mit den Vatriablen aus
+        dem Temperaturprofil kollidiert
+     *
+     * Über die multivariate Funktion der Geschwindigkeit wird mittels zweier for-Schleifen iteriert. Die Daten werden dann der QColorMap zugewiesen
+     */
+    double s;
+    double t;
+    double u;
     for (int sIndex=0; sIndex<ns; ++sIndex)
     {
       for (int tIndex=0; tIndex<ns; ++tIndex)
       {
+          // Testfunktion und wird noch ersetzt
         colorMap->data()->cellToCoord(sIndex, tIndex, &s, &t);
         double r = 3*qSqrt(s*s+t*t)+1e-2;
-        u = 2*s*(qCos(r+2)/r-qSin(r+2)/r);
+        u = 2*s*(qCos(r+2)/r-qSin(r+2)/r)+1;
         colorMap->data()->setCell(sIndex, tIndex, u);
       }
     }
 
-    // Hinzufügen der Farbskala
+    /// Hinzufügen der Farbskala
     QCPColorScale *colorScale = new QCPColorScale(ui->speedGraph);
-    ui->speedGraph->plotLayout()->addElement(0, 1, colorScale); // Skala wird der richtigen Achse zugewiesen
-    colorScale->setType(QCPAxis::atRight); // Vertikale Farbskala auf der rechten Seite
-    colorMap->setColorScale(colorScale); // Verbindung von Farbe der Skala mit der Farbe der ColorMap
-    colorScale->axis()->setLabel("Irgendwas Cooles");
 
-    // Es wird hier ein Farbgradient aus den Voreinstellungen geladen:
-    colorMap->setGradient(QCPColorGradient::gpPolar);
+    /// Skala wird der richtigen Achse zugewiesen
+    ui->speedGraph->plotLayout()->addElement(0, 1, colorScale);
 
-    // Die Farbdimension wird so angepasst, sodass alle erzeugten Datenpunkten auch eine Farbe zugewiesen bekommen
+    /// Vertikale Farbskala auf der rechten Seite
+    colorScale->setType(QCPAxis::atRight);
+
+    /// Verbindung von Farbe der Skala mit der Farbe der ColorMap
+    colorMap->setColorScale(colorScale);
+
+    /// Hinzufügen der Skalenbeschriftung
+    colorScale->axis()->setLabel("Geschwindigkeit in [m/s]");
+
+    /*!
+     * \brief Laden eines Farbgradient aus den Voreinstellung
+     *
+     * gpJet ist ein Fabrverlauf der für numerische Analysen verwendet wird und passt daher auf unseren Anwendungsfall
+     */
+    colorMap->setGradient(QCPColorGradient::gpJet);
+    /*!
+     * \brief Die Farbdimension wird so angepasst, sodass alle erzeugten Datenpunkten auch eine Farbe zugewiesen bekommen.
+     *
+     * Die Anpassung an die Werte geschieht automatisch, sodass die Bandbreite von der geringsten auftretenden Greschwindigkeit zur maximalen Geschwindigkeit geht.
+     */
     colorMap->rescaleDataRange();
 
-    // Achsen und ColorMap an sich sollen direkt aneinaderliegen:
+    /// Achsen und ColorMap an sich sollen direkt aneinaderliegen:
     QCPMarginGroup *marginGroup = new QCPMarginGroup(ui->speedGraph);
     ui->speedGraph->axisRect()->setMarginGroup(QCP::msBottom|QCP::msTop, marginGroup);
     colorScale->setMarginGroup(QCP::msBottom|QCP::msTop, marginGroup);
 
-    // Anpassung der Größe der Achsen an Widgetgröße:
+    /// Anpassung der Größe der Achsen an Widgetgröße:
     ui->speedGraph->rescaleAxes();
 
 
